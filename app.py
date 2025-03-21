2a836b2db28f4e73db201b1c8bf2d6f45f2a8b20
Reshma John <reshma.chackola@gmail.com>
2025-03-21 12:38:37 +0100

    table schema created in library.sqlite

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from data_models import db, Author, Book
+from data_models import db, Book
+import os
 
 
 #Initializing the flask.
 app = Flask(__name__)
 
+db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data', 'library.sqlite')
+
 #Setting the Database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
 
 #Used to connect the Flask app to the flask-sqlalchemy code.
 db.init_app(app)
+
+
+# Create the tables if they don't already exist
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
