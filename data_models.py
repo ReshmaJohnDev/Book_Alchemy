4bc5d42363a22c8768a943c5c7a22569e44398c0
Reshma John <reshma.chackola@gmail.com>
2025-03-21 12:36:44 +0100

    Added the Book model

--- data_models.py
+++ data_models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import foreign
 
 db = SQLAlchemy()
 
@@ -12,6 +13,18 @@ class Author(db.Model):
 
     def __repr__(self):
         return f"Author(id = {self.author_id}, name = {self.author_name})"
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    book_id = Column(Integer, primary_key=True, autoincrement=True)
+    book_isbn = Column(String)
+    book_title = Column(String)
+    book_publication_year = Column(Date)
+    author_id = Column(Integer, ForeignKey('authors.author_id') )
+
+    def __repr__(self):
+        return f"Book(id = {self.book_id}, title = {self.book_title})"
 
 
 
